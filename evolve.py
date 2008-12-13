08acd77f133e4e92febbbf8163df6ae04094ab40
Alex Gaynor <alex.gaynor@gmail.com>
2008-12-13 12:55:33 -0600

    switching to use pyglet

--- evolve.py
+++ evolve.py
@@ -1,36 +1,15 @@
-import gobject
-import gtk
-import gtk.glade
-import pygtk
+import pyglet
+
 
 IMAGE_FILE_NAME = 'mona_lisa_crop.jpg'
 
 class Evolves(object):
     def __init__(self):
-        self.gladefile = 'ui.glade'
-        self.wTree = gtk.glade.XML(self.gladefile, 'main_window')
-        self.wTree.signal_autoconnect(self)
-        
-        self.setup_widgets()
-            
-    def setup_widgets(self):
-        self.original = self.wTree.get_widget('real_image')
-        self.best = self.wTree.get_widget('best')
-        self.evolves = self.wTree.get_widget('evolves')
-        
-        self.img = gtk.gdk.pixbuf_new_from_file(IMAGE_FILE_NAME)
-        self.gc = self.original.window.new_gc()
-        
-        self.wTree.get_widget('main_window').resize(int(self.img.get_width()*3.5), int(self.img.get_height()*1.5))
-    
-    def orig_expose(self, *args):
-        self.original.window.draw_pixbuf(self.gc, self.img, 0, 0, 0, 0)
-    
-    def quit(self, *args, **kwargs):
-        gtk.main_quit()
+        pass
     
     def run(self):
-        gtk.main()
+        pyglet.app.run()
+    
 
 if __name__ == '__main__':
     Evolves().run()
